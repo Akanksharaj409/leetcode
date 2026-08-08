e9e2469e9222a6a75148e560961338df4eaf13f6
Akanksha Raj <akanksharajbkp2006@gmail.com>
2026-08-08 11:08:51 +0530

    Time: 32 ms (86.11%), Space: 137.4 MB (52.78%) - LeetHub

---
+++ 3302-find-the-lexicographically-smallest-valid-sequence/3302-find-the-lexicographically-smallest-valid-sequence.java
@@ -0,0 +1,46 @@
+class Solution {
+    public int[] validSequence(String word1, String word2) {
+        int n = word1.length();
+        int m = word2.length();
+
+        int[] suffix = new int[m];
+
+        for (int i = 0; i < m; i++) {
+            suffix[i] = -1;
+        }
+
+        int i = n - 1;
+        int j = m - 1;
+
+        while (i >= 0 && j >= 0) {
+            if (word1.charAt(i) == word2.charAt(j)) {
+                suffix[j] = i;
+                j--;
+            }
+            i--;
+        }
+
+        int[] ans = new int[m];
+
+        boolean mismatchUsed = false;
+        j = 0;
+
+        for (i = 0; i < n && j < m; i++) {
+            if (word1.charAt(i) == word2.charAt(j)) {
+                ans[j] = i;
+                j++;
+            } else if (!mismatchUsed &&
+                       (j == m - 1 || suffix[j + 1] > i)) {
+                ans[j] = i;
+                j++;
+                mismatchUsed = true;
+            }
+        }
+
+        if (j != m) {
+            return new int[0];
+        }
+
+        return ans;
+    }
+}
